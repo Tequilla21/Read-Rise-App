72ae0405d63ec1fd584a85d06814e00492606a43
Tequilla Terry <Tequilla1212@gmail.com>
2025-09-16 11:31:33 -0400

    Set Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👇 important for GitHub Pages (project page)
+  base: '/Read-Rise-App/', // replace with your repo name
 })
